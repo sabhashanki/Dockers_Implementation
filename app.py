3725925e13efd537336ed34d9b8877fed65a0493
sabhashanki <sabhashanki@gmail.com>
2022-10-16 21:00:12 +0530

    req file and docker file updated

--- app.py
+++ app.py
@@ -1,4 +1,12 @@
 import pandas as pd
+from flask import Flask, request, render_template
+
 df = pd.DataFrame([1,2,3,3,5], columns={'numbers'})
 
-@app:route
+app = Flask(__name__)
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+if __name__ == '__main__':
+    app.run(debug = True)
